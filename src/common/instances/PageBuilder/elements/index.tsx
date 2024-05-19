c81425113403dfcd95f4b453b8de01a8e61aac1a
Luiz Felipe Moureau <xfazxtudo@gmail.com>
2024-05-19 04:56:37 -0300

    :sparkles: feat: allowed SSR on nested instances

--- src/common/instances/PageBuilder/elements/index.tsx
+++ src/common/instances/PageBuilder/elements/index.tsx
@@ -1,12 +1,22 @@
-import Nullstack from 'nullstack';
+import Nullstack, { NullstackClientContext } from 'nullstack';
 
 import { Mococa } from '@types';
 
-export class ElementsInstances extends Nullstack {
+interface Props {
+  onref: (ref: ElementsInstances) => void;
+}
+
+export class ElementsInstances extends Nullstack<Props> {
   elements: Mococa.BuildComponent[] = [];
   selected_element: string;
 
-  prepare(props) {
-    console.log({ props });
+  prepare(context: NullstackClientContext<Props>) {
+    context.onref(this);
+  }
+
+  update() {
+    if (this.elements.length === 0) {
+      this.selected_element = null;
+    }
   }
 }
--- src/common/instances/PageBuilder/index.tsx
+++ src/common/instances/PageBuilder/index.tsx
@@ -1,7 +1,17 @@
-import Nullstack from 'nullstack';
+import Nullstack, { NullstackClientContext } from 'nullstack';
 
 import { ElementsInstances } from './elements';
 
 export class PageBuilderInstance extends Nullstack {
   elements: ElementsInstances;
+
+  initiate({ instances }: NullstackClientContext) {
+    instances.pagebuilder.elements = this.elements;
+  }
+
+  render() {
+    return (
+      <ElementsInstances persistent onref={ref => (this.elements = ref)} />
+    );
+  }
 }
